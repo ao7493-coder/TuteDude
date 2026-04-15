ce5969adedfc80f65358b73558773d36a0ab81d2
ao7493-coder <ao7493@srmist.edu.in>
2026-04-15 14:50:03 +0530

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,26 @@
+# Function Addtion
+def addNumbers(x,y):
+    return x + y
+
+# Function for subtraction
+def subtractNumbers(x, y):
+
+    return x - y
+
+# Function for multiplication
+def multiplication(x,y):
+    return x*y
+
+#Function for division
+def division(x,y):
+    return x/y
+
+x = int(input("Enter the number : "))
+y = int(input("Enter the number : "))
+
+#calling all the Functions !!!
+print("Addition :",addNumbers(x,y))
+print("Subtraction ",subtractNumbers(x,y))
+print("Multiplication",multiplication(x,y))
+print("Division",division(x,y))
+
